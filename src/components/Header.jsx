e72ce6390bebd19616a73215b00a2e879423b546
Arjunlama2 <instructorarjun1@gmail.com>
2025-08-01 18:04:50 +0545

    frontend code

---
+++ src/components/Header.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import MobileNav from "./MobileNav";
+import { RxCross2 } from "react-icons/rx";
+
+function Header() {
+  const [isMobile,setMobile]=useState(false)
+  return (
+    <>
+      <header className="flex justify-between mx-6 items-center h-16 ">
+        <img src="/logo.png" alt="" className="" />
+        <nav className="hidden md:flex list-none gap-[32px] cursor-pointer">
+          <li className="">About</li>
+          <li>Features</li>
+          <li>Pricing</li>
+          <li>Testimonials</li>
+          <li>Help</li>
+        </nav>
+        <div className="hidden md:flex ">
+          <button className="font-[500]   rounded-2xl border-2 border-white px-10  py-2 hover:text-[#F53855] hover:border-2 hover:border-[#F53855]">
+            Sign In{" "}
+          </button>
+          <button className="font-[500]   rounded-2xl border-2 border-white px-10  py-2 hover:text-[#F53855] hover:border-2 hover:border-[#F53855]">
+            Sign Up
+          </button>
+        </div>
+        {
+          isMobile?<RxCross2 className="md:hidden text-3xl"   onClick={()=>setMobile(false)} />:  <GiHamburgerMenu className="md:hidden text-3xl"  onClick={()=>setMobile(true)}/>
+        }
+      
+
+        {isMobile && <MobileNav />}
+      </header>
+    </>
+  );
+}
+
+export default Header;
---
+++ src/components/MobileNav.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function MobileNav() {
+  return (
+    <div className="md:hidden fixed top-12   bg-black/70 w-full h-[100vh] ">
+      <div className="absolute right-0 flex flex-col gap-5 items-center w-1/2 h-full bg-red-600 text-white   ">
+        <nav className=" cursor-pointer list-none flex flex-col gap-5">
+          <li className="">About</li>
+          <li>Features</li>
+          <li>Pricing</li>
+          <li>Testimonials</li>
+          <li>Help</li>
+        </nav>
+        <div className="flex flex-col gap-5">
+          <button className="font-[500]   rounded-2xl border-2 border-white px-10  py-2 hover:text-[#F53855] hover:border-2 hover:border-[#F53855]">
+            Sign In{" "}
+          </button>
+          <button className="font-[500]   rounded-2xl border-2 border-white px-10  py-2 hover:text-[#F53855] hover:border-2 hover:border-[#F53855]">
+            Sign Up
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MobileNav;
